264e13b3000ac3302d73c16e063d462b4f47a90b
vadim snigirev <snvad@mail.ru>
2022-12-16 19:30:29 +0300

    Ugadai random

---
+++ task_001/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = new Random().Next(0,101);
+Console.WriteLine("Напишите число от 0 до 100");
+int usernum = System.Convert.ToInt32(Console.ReadLine());
+if (usernum == a)
+{
+    Console.WriteLine("Да ты Ванга!");
+}
+else
+{
+    Console.Write("Повезет в другой раз, компьютер загадал ");
+    Console.WriteLine(a);
+}
